3c517eb4fbef535e1284ab405878b46033340b91
alexander <a.valov1705@gmail.com>
2020-10-23 21:27:27 +0700

    added testing environment

---
+++ Tests/TestRK45/src/TestRungeKutta45.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+
+#include <iostream>
+
+TEST(TestRungeKutta45, ExponentTest) {
+    std::cout << "test228\n";
+}
+
+TEST(TestRungeKutta45, LinearHomogeneousTest) {
+    std::cout << "test69\n";
+}
+
+TEST(TestRungeKutta45, LinearInhomogeneousTest) {
+    std::cout << "test69\n";
+}
